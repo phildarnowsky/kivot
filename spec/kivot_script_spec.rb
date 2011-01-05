5f326abf8a7428ac8fd545551f9b93df7b81b19c
Phil Darnowsky <phil@metamorph.(none)>
2011-01-04 21:22:19 -0500

    can now specify token in .kivot_token config file

--- spec/kivot_script_spec.rb
+++ spec/kivot_script_spec.rb
@@ -10,7 +10,7 @@ describe "kivot script" do
   end
 
   def make_mock_poster
-    mock_poster = mock
+    mock_poster = mock(:post_story => nil)
     Kivot::PivotalPoster.stub!(:new).and_return(mock_poster)
 
     mock_poster
@@ -44,6 +44,95 @@ describe "kivot script" do
       Kivot::PivotalPoster.should_receive(:new).with(@fake_token, anything).and_return(stub(:post_story => nil))
 
       run_script
+    end
+  end
+
+  context "when no token argument is present" do
+    before(:each) do
+      set_argv(["-p", @fake_project_id, @fake_name])
+
+      @current_path = Pathname.new("/data/projects/turnip_twaddler")
+      Pathname.stub(:getwd).and_return(@current_path)
+    end
+
+    context "but a .kivot_token file is present" do
+      # A lot of the setup below probably constitutes mock and stub abuse, but
+      # it's hard to see how to do it with real Pathnames--we don't want to
+      # make any assumptions about the user's filesystem.
+
+      before(:each) do
+        @alternate_fake_token = 'b123b123'
+      end
+
+      context "in the current directory" do
+        before(:each) do
+          @mock_path = mock
+          @current_path.stub(:join).with('.kivot_token').and_return(@mock_path)
+          @mock_path.stub(:file?).and_return(true)
+
+          File.stub(:read).with(@mock_path).and_return(@alternate_fake_token)
+        end
+
+        it "should use the token in that file" do
+          Kivot::PivotalPoster.should_receive(:new).with(@alternate_fake_token, anything).and_return(make_mock_poster)
+
+          run_script
+        end
+      end
+
+      context "in a parent directory" do
+        before(:each) do
+          @mock_found_path = mock(:file? => true)
+          File.stub(:read).with(@mock_found_path).and_return(@alternate_fake_token)
+
+          @mock_grandparent = mock(:join => @mock_found_path)
+          @mock_parent = mock(:parent => @mock_grandparent, :join => mock(:file? => false), :root? => false)
+          @mock_path = mock(:file? => false, :root? => false)
+
+          @current_path.stub(:join).and_return(@mock_path)
+          @current_path.stub(:parent).and_return(@mock_parent)
+        end
+
+        it "should use the token in that file" do
+          Kivot::PivotalPoster.should_receive(:new).with(@alternate_fake_token, anything).and_return(make_mock_poster)
+
+          run_script
+        end
+      end
+
+      context "in the user's home directory" do
+        before(:each) do
+          @mock_found_path = mock(:file? => true)
+          File.stub(:read).with(@mock_found_path).and_return(@alternate_fake_token)
+
+          @mock_home_dirname = mock
+          @mock_home_path = mock(:join => @mock_found_path)
+          File.stub(:expand_path).with('~').and_return(@mock_home_dirname)
+          Pathname.stub(:new).with(@mock_home_dirname).and_return(@mock_home_path)
+
+          @mock_parent = mock(:join => mock(:file? => false), :root? => true)
+          @mock_path = mock(:file? => false)
+
+          @current_path.stub(:join).and_return(@mock_path)
+          @current_path.stub(:parent).and_return(@mock_parent)
+        end
+
+        it "should use the token in that file" do
+          Kivot::PivotalPoster.should_receive(:new).with(@alternate_fake_token, anything).and_return(make_mock_poster)
+
+          run_script
+        end
+      end
+    end
+
+    context "and no .kivot_token file is present in an expected location" do
+      before(:each) do
+        stub(:find_file_in_home => nil)
+      end
+
+      it "should exit" do
+        lambda{run_script}.should raise_exception(SystemExit)
+      end
     end
   end
 
